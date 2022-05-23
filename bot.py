302b5fea7fc5f43a3be53ca72c06bf1697023a9b
strikerop96 <106062852+strikerop96@users.noreply.github.com>
2022-05-24 01:03:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
 
 ###USE YOUR ROTATING PROXY### NEED HQ PROXIES ELSE WONT WORK UPDATE THIS FILED
-r = requests.get('https://api.proxyscrape.com/v2/account/datacenter_shared/proxy-list?auth=m4aesaoyjvgf1at2rtin&type=getproxies&country[]=all&protocol=http&format=normal&status=all')
+r = requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all')
 res = r.partition('\n')[0]
 proxy = {"http": f"http://{res}"}
 session = requests.session()
